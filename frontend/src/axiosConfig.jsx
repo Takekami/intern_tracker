a6b10238dd1a658cea1eb5f4e1bdc8f6fe1040ed
Takekami <tkyk.murakami@gmail.com>
2025-08-04 18:13:49 +1000

    fix the frontend dependencies error

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
+  // baseURL: 'http://localhost:5001', // local
   baseURL: 'http://13.210.66.196', // live
   headers: { 'Content-Type': 'application/json' },
 });
