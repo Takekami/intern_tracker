ce69df8810eace0cc6b865c7f0b82daac7cfe657
Takekami <tkyk.murakami@gmail.com>
2025-08-04 18:09:09 +1000

    change the directory of .github

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://3.26.96.188:5001', // live
+  baseURL: 'http://13.210.66.196', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
