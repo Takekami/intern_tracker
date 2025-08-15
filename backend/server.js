6df032ec5d2388eebcb2b041a2eb42cd0ebfb72d
Takekami <tkyk.murakami@gmail.com>
2025-08-15 19:51:40 +1000

    change public IP address

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,10 @@
-const path = require('path');
-require('dotenv').config({
-  path: path.join(__dirname, '.env'),
-});
-
 const express = require('express');
+const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const reportRoutes = require('./routes/reportRoutes');
 
-if (!process.env.MONGO_URI) {
-  console.error('FATAL: MONGO_URI is not loaded from .env');
-  process.exit(1);
-}
+dotenv.config();
 
 const app = express();
 
@@ -24,9 +17,11 @@ app.use('/api/reports', reportRoutes);
 
 // Export the app object for testing
 if (require.main === module) {
-  connectDB();
-  const PORT = process.env.PORT || 5001;
-  app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
-}
+    connectDB();
+    // If the file is run directly, start the server
+    const PORT = process.env.PORT || 5001;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  }
 
-module.exports = app;
+
+module.exports = app
