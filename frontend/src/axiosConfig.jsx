ee5cb28e7b50f7e3d328a47cb850d7d395ea86a7
Takekami <tkyk.murakami@gmail.com>
2025-08-15 17:26:44 +1000

    modify yml file

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://52.64.16.113:5001', // live
+  baseURL: 'http://16.176.160.173:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
